022b1d7c7f481317fd811de96c65da721e0eaac1
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-26 08:54:55 +0800

    Add assignment 012 answers

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,26 @@
+/* 
 let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 
 console.log(40 + 8 + 23 - 10);
 
-// ASSIGNMENTS - Values And Variables
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 'ytjt');
+console.log(typeof 23);
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1998;
+console.log(year);
+console.log(typeof year);
+console.log(typeof null);
+*/
+
+// Assignment - Values And Variables
+
 let country = 'South Korea';
 let continent = 'Asia';
 let population = 51780000;
@@ -11,3 +28,13 @@ let population = 51780000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Assignment - Data Types
+
+let isIsland = true;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
