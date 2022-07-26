f95b0c405e15eba79a021089dfe53113cf43dcae
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-26 08:24:12 +0800

    Add assignment answers

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,13 @@
-      let js = 'amazing';
-      if (js === 'amazing') alert('JavaScript is FUN!');
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
 
-      console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
+
+// ASSIGNMENTS - Values And Variables
+let country = 'South Korea';
+let continent = 'Asia';
+let population = 51780000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
