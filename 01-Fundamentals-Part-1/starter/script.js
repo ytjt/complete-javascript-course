1f0808d123cc0f648d8c413dfdbdce47d23f59ca
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-26 10:14:08 +0800

    Add assignment 013 and 014 answer

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -17,9 +17,38 @@ year = 1998;
 console.log(year);
 console.log(typeof year);
 console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
+
+const job;
+
+const now = 2022;
+const ageYtjt = now - 2000;
+console.log(ageYtjt);
+console.log(ageYtjt * 10, ageYtjt / 10, 2 ** 3); // 2**3 means 2 to the power of 3. 2^3. 2 * 2 * 2.
+
+const firstName = 'Ytjt';
+const lastName = 'iykyk';
+console.log(firstName + lastName);
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+console.log(x);
+const ageIykYk = now - 2001;
+console.log(ageYtjt > ageIykYk);
+console.log(ageIykYk >= 30);
+
+const isAdult = ageIykYk >= 18;
 */
 
-// Assignment - Values And Variables
+// ============================ ASSIGNMENTS ============================
+
+// Assignment 011 - Values And Variables
 
 let country = 'South Korea';
 let continent = 'Asia';
@@ -29,12 +58,36 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-//Assignment - Data Types
+//Assignment 012 - Data Types
 
-let isIsland = true;
+const isIsland = true;
 let language;
 
 console.log(isIsland);
 console.log(population);
 console.log(country);
 console.log(language);
+
+// Assignment 013 - let, const and var
+
+language = 'Hangul';
+// isIsland = false;
+
+// Assignment 014 - Basic Operators
+
+console.log(population / 2);
+population += 1;
+console.log(population);
+const populationFinland = 6000000;
+console.log(population > populationFinland);
+const avgPopulation = 33000000;
+console.log(population < avgPopulation);
+const description =
+  population +
+  ' is in ' +
+  continent +
+  ', and its ' +
+  population +
+  ' people speak ' +
+  language;
+console.log(description);
