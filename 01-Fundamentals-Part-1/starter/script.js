de56229512fcb96f6e4cb4a3bab813f3b60520a7
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-26 10:52:44 +0800

    Add coding challenge #1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -47,7 +47,7 @@ const isAdult = ageIykYk >= 18;
 */
 
 // ============================ ASSIGNMENTS ============================
-
+/* 
 // Assignment 011 - Values And Variables
 
 let country = 'South Korea';
@@ -91,3 +91,40 @@ const description =
   ' people speak ' +
   language;
 console.log(description);
+ */
+// ========================= CODING CHALLENGE =========================
+
+// Coding Challenge #1
+
+//Data 1
+const markMass1 = 78;
+const markHeight1 = 1.69;
+
+const johnMass1 = 92;
+const johnHeight1 = 1.95;
+
+//Calculate BMI 1
+const bmiMark1 = markMass1 / markHeight1 ** 2;
+const bmiJohn1 = johnMass1 / johnHeight1 ** 2;
+
+//BMI comparison 1
+let markHigherBMI;
+markHigherBMI = bmiMark1 > bmiJohn1;
+console.log(bmiMark1, bmiJohn1);
+console.log(markHigherBMI);
+
+//Data 2
+const markMass2 = 95;
+const markHeight2 = 1.88;
+
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
+
+//Calculate BMI 2
+const bmiMark2 = markMass2 / markHeight2 ** 2;
+const bmiJohn2 = johnMass2 / johnHeight2 ** 2;
+
+//BMI Comparison 2
+markHigherBMI = bmiMark2 > bmiJohn2;
+console.log(bmiMark2, bmiJohn2);
+console.log(markHigherBMI);
