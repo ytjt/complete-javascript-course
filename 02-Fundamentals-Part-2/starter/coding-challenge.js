6c92937ae4067f9bace659f0a9e0a42377254c03
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-28 22:10:12 +0800

    Edit coding-challenge.js for cc #1

--- 02-Fundamentals-Part-2/starter/coding-challenge.js
+++ 02-Fundamentals-Part-2/starter/coding-challenge.js
@@ -3,15 +3,21 @@
 
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 2;
 
-// Average score
+// Test Data 1
 const avgDolphins = calcAverage(44, 23, 71);
-const avgKoalas = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(65, 54, 49);
+
+// Test Data 2
+// const avgDolphins = calcAverage(85, 54, 41);
+// const avgKoalas = calcAverage(23, 34, 27);
 
 function checkWinner(avgDolphins, avgKoalas) {
   if (avgDolphins >= avgKoalas * 2) {
     console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
-  } else {
+  } else if (avgKoalas >= avgDolphins * 2) {
     console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log('No one wins huhu');
   }
 }
 
