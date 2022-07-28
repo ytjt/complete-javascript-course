e60139cde61519c9d0717e938d217c9ba44ec1c6
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-28 20:43:37 +0800

    Edit asg.js

--- 02-Fundamentals-Part-2/starter/asg.js
+++ 02-Fundamentals-Part-2/starter/asg.js
@@ -4,4 +4,6 @@ function describeCountry(country, population, capitalCity) {
   return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
 }
 
-console.log(describeCountry('Malaysia', '33', 'Kuala Lumpur'));
+console.log(describeCountry('Malaysia', '33.8', 'Kuala Lumpur'));
+console.log(describeCountry('South Korea', '51.98', 'Seoul'));
+console.log(describeCountry('Japan', '124.9', 'Tokyo'));
