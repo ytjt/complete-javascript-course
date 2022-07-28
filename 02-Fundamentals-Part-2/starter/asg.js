ca8052f7a9afccb0ca2fc730730b958088a8fd73
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-28 21:35:47 +0800

    Edit asg.js

--- 02-Fundamentals-Part-2/starter/asg.js
+++ 02-Fundamentals-Part-2/starter/asg.js
@@ -1,9 +1,31 @@
 /* ----- LECTURE 01: FUNCTIONS ----- */
 
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+// }
+
+// console.log(describeCountry('Malaysia', 33.8, 'Kuala Lumpur'));
+// console.log(describeCountry('South Korea', 51.98, 'Seoul'));
+// console.log(describeCountry('Japan', 124.9, 'Tokyo'));
+
+/* ----- LECTURE 02: FUNCTION DECLARATIONS VS EXPRESSION ----- */
+
+//FUNCTION DECLARATION
+function percentageOfWorld1(population) {
+  const worldPopulation = 7900;
+  return `${(population / worldPopulation) * 100}%`;
 }
 
-console.log(describeCountry('Malaysia', '33.8', 'Kuala Lumpur'));
-console.log(describeCountry('South Korea', '51.98', 'Seoul'));
-console.log(describeCountry('Japan', '124.9', 'Tokyo'));
+console.log(percentageOfWorld1(33.8));
+console.log(percentageOfWorld1(51.98));
+console.log(percentageOfWorld1(124.9));
+
+//FUNCTION EXPRESSION
+const percentageOfWorld2 = function (population) {
+  const worldPopulation = 7900;
+  return `${(population / worldPopulation) * 100}%`;
+};
+
+console.log(percentageOfWorld1(33.8));
+console.log(percentageOfWorld1(51.98));
+console.log(percentageOfWorld1(124.9));
