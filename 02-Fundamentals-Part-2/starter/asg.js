c60dff104e94366a395902269eb1697c0c82bea4
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-28 20:38:45 +0800

    Edit asg.js

---
+++ 02-Fundamentals-Part-2/starter/asg.js
@@ -0,0 +1,7 @@
+/* ----- LECTURE 01: FUNCTIONS ----- */
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+}
+
+console.log(describeCountry('Malaysia', '33', 'Kuala Lumpur'));
