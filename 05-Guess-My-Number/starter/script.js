3ed736af9d63346db3b7ea6ccaec12f0458feebe
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-29 19:42:50 +0800

    Edit script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,14 +15,16 @@ document.querySelector('.guess').value = 21;
 console.log(document.querySelector('.guess').value); */
 
 /* ------------------------- HANDLING CLICK EVENTS ------------------------- */
-// Event - something that happens on the page (mouse click, mouse moving, keypress etc.)
-//.addEventListener(name of the event that we are listening for, function value);
+// SCORE TO BE MUTATED
+let score = 20; // state variable (part of application state)
 
+// THE SECRET NUMBER
 const secretNumber = Math.trunc(Math.random() * 20 + 1); // gives number between 0 and 1, Math.trunc() to remove the decimals, * 20 because we want 0 - 20, + 1 is to elevate until 20 (or else max is 19.99999999999999 and remove d.p will just give until 19)
 
-let score = 20; // state variable (part of application state)
 document.querySelector('.number').textContent = secretNumber;
 
+// EVENT - something that happens on the page (mouse click, mouse moving, keypress etc.)
+//.addEventListener(name of the event that we are listening for, (what to do - event handler) function value);
 document.querySelector('.check').addEventListener('click', function () {
   //   console.log(document.querySelector('.guess').value);
 
@@ -31,18 +33,30 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     // empty input = 0 = is a falsy value
-    document.querySelector('.message').textContent = '❌ NO NUMBER';
+    document.querySelector('.message').textContent = 'NO NUMBER?! >:(';
     score--;
     document.querySelector('.score').textContent = score;
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'CORRECT! :D';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '❌ TOO HIGH';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'ITS TOO HIGH >.<';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'YOU LOST THE GAME. BRUH?!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '❌ TOO LOW';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'ITS TOO LOW >.<';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'YOU LOST THE GAME. BRUH?!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
