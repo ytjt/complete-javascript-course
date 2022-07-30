ddc22ffba981df767b8c743c23090ba4235f1f5e
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-30 21:52:32 +0800

    Fix bug
    At first, the score wasn't really resetting to 20 and just continue from previous session.
    It was due to the score not being initialized back to 20 and was just manipulating the DOM. woops

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -79,7 +79,9 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.score').textContent = 20;
+  // document.querySelector('.score').textContent = 20;
+  score = 20;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
