c85b84c32015722c809ec22cb109039a613a18d6
ytjt <102714570+ytjt@users.noreply.github.com>
2022-07-31 20:38:07 +0800

    Coding challenge #3

--- 02-Fundamentals-Part-2/starter/coding-challenge.js
+++ 02-Fundamentals-Part-2/starter/coding-challenge.js
@@ -32,16 +32,46 @@
 // };
 
 // Arrow function
-const calcTip = (bill) =>
-  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = (bill) =>
+//   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-console.log(calcTip(100));
+// console.log(calcTip(100));
 
-const bills = [125, 555, 44];
-console.log(`Bills: ${bills}`);
+// const bills = [125, 555, 44];
+// console.log(`Bills: ${bills}`);
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(`Tips: ${tips}`);
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(`Tips: ${tips}`);
 
-const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
-console.log(`Total paid: ${total}`);
+// const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+// console.log(`Total paid: ${total}`);
+
+/* ----- CODING CHALLENGE #3 ----- */
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+console.log(
+  mark.calcBMI() > john.calcBMI()
+    ? `${mark.fullName}'s BMI (${mark.bmi})is higher than ${john.fullName}'s (${john.bmi})!`
+    : `${john.fullName}'s BMI (${john.bmi})is higher than ${mark.fullName}'s (${mark.bmi})!`
+);
