ae73c7c07f0251ee3d8e387b0d343ab1caf86cf6
ytjt <102714570+ytjt@users.noreply.github.com>
2022-08-01 14:39:34 +0800

    Add event listener for close modal buttons and overlay

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,11 +2,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-const btnCloseModal = document.querySelector('.close-overlay');
+const btnCloseModal = document.querySelector('.close-modal');
 
 for (let i = 0; i < btnOpenModal.length; i++) {
   btnOpenModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
+  });
+
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+
+  overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
   });
 }
