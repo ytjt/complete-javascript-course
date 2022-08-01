904723be27e69a7f0e7823dc9d42c6ba86b83171
ytjt <102714570+ytjt@users.noreply.github.com>
2022-08-01 18:04:44 +0800

    Add currScore initializer and updater

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,8 +4,8 @@ const elementScore0 = document.querySelector('#score--0');
 const elementScore1 = document.getElementById('score--1');
 const elementDice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
-const currScore0 = document.getElementById('current--0');
-const currScore1 = document.getElementById('current--1');
+const elCurrScore0 = document.getElementById('current--0');
+const elCurrScore1 = document.getElementById('current--1');
 
 elementScore0.textContent = '0';
 
@@ -13,13 +13,15 @@ elementScore1.textContent = '0';
 
 elementDice.classList.add('hidden');
 
+let currScore = 0;
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   elementDice.classList.remove('hidden');
   elementDice.src = `dice-${dice}.png`;
 
   if (dice !== 1) {
-    currScore0.textContent = dice;
+    currScore += dice;
+    elCurrScore0.textContent = currScore;
   } else {
   }
 });
