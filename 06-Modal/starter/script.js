b3f5ba39e4563e53feb9556b27ce2694aca4db7f
ytjt <102714570+ytjt@users.noreply.github.com>
2022-08-01 14:42:08 +0800

    Refactor code. Added openModal and closeModal functions

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,19 +4,20 @@ const overlay = document.querySelector('.overlay');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 
-  btnCloseModal.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
+  btnCloseModal.addEventListener('click', closeModal);
 
-  overlay.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
+  overlay.addEventListener('click', closeModal);
 }
