9b8b3ae85e5ef37fd2d30b45c2d8af7a4ee53717
ytjt <102714570+ytjt@users.noreply.github.com>
2022-08-01 17:48:10 +0800

    Add rolling dice functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const elementScore0 = document.querySelector('#score--0');
+const elementScore1 = document.getElementById('score--1');
+const elementDice = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+
+elementScore0.textContent = '0';
+
+elementScore1.textContent = '0';
+
+elementDice.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  elementDice.classList.remove('hidden');
+  elementDice.src = `dice-${dice}.png`;
+});
