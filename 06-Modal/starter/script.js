56f7f8b0b85d0bf5b11bd41c621e9204a0a73c7d
ytjt <102714570+ytjt@users.noreply.github.com>
2022-08-01 13:47:52 +0800

    Edit script.js for modal window lecture video

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-overlay');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i]);
+}
