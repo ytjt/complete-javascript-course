9d67a0a7f2700c28900be494bf3b851158aa916f
ytjt <102714570+ytjt@users.noreply.github.com>
2022-08-01 18:00:15 +0800

    Add conditions for if dice !==1

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,6 +4,8 @@ const elementScore0 = document.querySelector('#score--0');
 const elementScore1 = document.getElementById('score--1');
 const elementDice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
+const currScore0 = document.getElementById('current--0');
+const currScore1 = document.getElementById('current--1');
 
 elementScore0.textContent = '0';
 
@@ -15,4 +17,9 @@ btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   elementDice.classList.remove('hidden');
   elementDice.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currScore0.textContent = dice;
+  } else {
+  }
 });
